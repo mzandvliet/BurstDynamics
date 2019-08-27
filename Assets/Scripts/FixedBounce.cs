47cad5c94565f7e7121fc3a64a83a93dfb99cd0a
m_zandvliet <martijn@ramjetanvil.com>
2019-08-27 22:11:15 +0200

    Mixed arithmetic for the little 2d critters. Velocity is now represented by 8 bits.

--- Assets/Scripts/FixedBounce.cs
+++ Assets/Scripts/FixedBounce.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec5a3eac8f1f71386f71989d44e2bd3061dbd80ec02be09d62f3120cc863d13a
-size 5950
+oid sha256:f9bcca57871a9899e266a553d0a2d74ca0b30cdd29e5bc6547ee2b1abcc519dc
+size 7124
