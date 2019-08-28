67806d797373320424eaa4d640c7365d730ac725
m_zandvliet <martijn@ramjetanvil.com>
2019-08-28 11:30:57 +0200

    Pretty good 8-bit particle motion now. Using new syntax for operations directly involving ints. Fixed a shift in the wrong direction when applying velocity. Identified a rounding bug in division which we'll tackle next.

--- Assets/Scripts/FixedBounce.cs
+++ Assets/Scripts/FixedBounce.cs
@@ -1,3 +1,241 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f9bcca57871a9899e266a553d0a2d74ca0b30cdd29e5bc6547ee2b1abcc519dc
-size 7124
+using UnityEngine;
+using Unity.Burst;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Mathematics;
+using Rng = Unity.Mathematics.Random;
+using Ramjet.Math.FixedPoint;
+using Ramjet.Math.LinearAlgebra;
+
+/*
+    First simple test where we write some physics stuff
+    with Fixed Point. Observations to be made along the way.
+
+    ** Physical State Representation **
+
+    Using the same qantizations for positions and velocity no longer make sense.
+
+    vec2_q23_8 might make sense for position data, but using the
+    same type for modest velocities in this space means you'll have
+    low precision and wasted bits.
+
+    After all, a large velocity in vec2_q23_8 would mean that the particle
+    traverses the space from top to bottom in a single tick.
+
+    ** State Update **
+
+    Delta-time is a weird notion, computationally. Specifically the
+    practise where you take a physical value, some energy, and you
+    multiply it by a fractional number to scale it down to a smaller 
+    window of time. 
+    
+    With infinite-precision numbers, or something that approaches that,
+    you can pretend that your choice of delta time doesn't matter. But
+    with these limited-precision fixed point numbers we find that choice
+    of such deltaTime makes a world of difference.
+
+    Having said that, when integrating you might not want to instantly
+    add all your velocity to position, only a portion.
+
+    So how do we deal with this? There must be multiple strategies.
+
+    1. Don't have a energy * deltaTime step
+    
+    If you want to scale by time in a way that doesn't wreck numerical
+    havok, you really have to pre-integrate this factor into the energies
+    you calculate, I think.
+
+    2. ??
+
+    This leads to:
+
+    ** Mixed precision arithmetic **
+
+    The easiest way to support some mixed-precision arithmetic is
+    to allow types with the same scale to interact, regardless
+    of wordlength.
+
+    E.g. you can freely add the following to a vec2_q24_7:
+    - vec2_q0_7
+    - vec2_q8_7
+    - vec2_q24_7
+
+    Unrelated: heh, we have a q24_7 type, nice.
+
+    Edit: We've implemented a first round of this for scalars,
+    and while it needs extra work and tooling, it works quite
+    well, even for non-equal fractional types.
+
+    ** Initialization **
+
+    Make a Random Number Generator that works for all Fixed types.
+
+    rng.NextQ24_7(0q, 14q);
+
+    We want better ways to initialize numbers in meaningful ways
+    without first expressing a number as an integer or float.
+
+    One way is to take a known yardstick and say: gimme 1/10 of
+    that.
+
+    q24_7.one is a useful yardstick, for sure.
+
+    So make that real easy.
+
+    ** Literals **
+
+    Is it at all possible to build support for custom literals into
+    this? Kind of sucks to have to type "+ new q24_7(1)" when you
+    mean to add 1 to something.
+
+    ** Type Conversion **
+
+    Implement explicit casts.
+
+    ** Range Tracking **
+
+    Want textual and visual indication of min/max, and precision.
+    Are we safe? Where would space actually wrap? How
+    much precision is left after this division?
+
+    ** Syntax Sugar **
+
+    TypeDeffing your favorite fixed point types per document
+    makes it easier to work with them.
+
+    ---
+
+    UNDERFLOW: Figure out conditions for when it will happen,
+    devise some strategies for avoiding it.
+
+    ---
+
+    Beyond the Basics
+
+    I want types that actually go way beyond the qn_m types
+    I have now.
+
+    When doing highly quantized physics, I want to track
+    velocity with 8 bits of mostly fraction, but for
+    an 8 bit position I want only 3 fraction bits or
+    less.
+
+    I might often now want the result of a multiplication
+    to be downshifted.
+ */
+
+using pscalar = Ramjet.Math.FixedPoint.qs3_4;
+using position = Ramjet.Math.LinearAlgebra.vec2_qs3_4;
+using vscalar = Ramjet.Math.FixedPoint.qs1_6;
+using velocity = Ramjet.Math.LinearAlgebra.vec2_qs1_6;
+
+public class FixedBounce : MonoBehaviour {
+    private NativeList<Particle> _particles;
+    private const int NumParticles = 1024;
+    private Rng _rng;
+    
+    private void Awake() {
+        _particles = new NativeList<Particle>(NumParticles, Allocator.Persistent);
+
+        _rng = new Rng(1234);
+
+        for (int i = 0; i < NumParticles; i++) {
+            _particles.Add(new Particle() {
+                position = position.FromFloat(_rng.NextFloat(-1f, 1f), _rng.NextFloat(-1f, 1f)),
+                velocity = velocity.FromInt(0, 0),
+            });
+        }
+
+        // var friction = q24_7.One - q24_7.Epsilon;
+        var friction = pscalar.FromFloat(0.9f);
+
+        /*
+        Problem: With friction values [0,1), and very small timesteps,
+        we get that velocity never goes to zero.
+
+        Note that close to zero, the ratio between successively smaller
+        numbers in the sequences becomes linear.
+
+        We get different results between these two approaches:
+        velocity = velocity * (One-Epsilon)
+        velocity = velocity / (One+Epsilon)
+
+        The multiplicative version yields UNDERFLOW of a kind, such
+        that velocity never goes to zero.
+        */
+
+        // Debug.Log("2.0 * 1.5 = " + (q24_7.FromFloat(2f) * q24_7.FromFloat(1.5f)));
+        // Debug.Log("-0.5f * 0.99f = " + (q24_7.FromFloat(-0.5f) * q24_7.FromFloat(0.99f)));
+
+        // var myVec = vec2_q24_7.FromFloat(9.5f, -1.2f);
+        // for (int i = 0; i < 128; i++) {
+        //     myVec *= friction;
+        //     Debug.Log(myVec.x.v);
+        // }
+
+        // myVec = vec2_q24_7.FromFloat(-9.5f, -1.2f);
+        // for (int i = 0; i < 128; i++) {
+        //     myVec *= friction;
+        //     Debug.Log(myVec.x.v);
+        // }
+    }
+
+    private void OnDestroy() {
+        _particles.Dispose();
+    }
+
+    // private static readonly vscalar deltaTime = new vscalar(24);
+
+    private void Update() {
+        var frictionMul = vscalar.One - vscalar.Epsilon;
+        var frictionDiv = vscalar.One + vscalar.Epsilon * 4;
+        // var frictionMul = scalar.FromFloat(0.9f);
+        // var frictionDiv = scalar.FromFloat(1.1f);
+
+        for (int i = 0; i < _particles.Length; i++) {
+            var p = _particles[i];
+
+            var nudge = new velocity(
+                new vscalar((sbyte)_rng.NextInt(-3, 4)),
+                new vscalar((sbyte)_rng.NextInt(-3, 4)));
+
+            p.velocity.x += nudge.x;
+            p.velocity.y += nudge.y;
+
+            // p.velocity *= frictionMul; // Nope...
+
+            // bug: division doesn't round properly, causing a biased motion to bottom left
+            p.velocity.x /= frictionDiv;
+            p.velocity.y /= frictionDiv;
+
+            var scaledVelocity = new velocity(p.velocity.x, p.velocity.y);
+
+            p.position.x += scaledVelocity.x;
+            p.position.y += scaledVelocity.y;
+
+            _particles[i] = p;
+        }
+    }
+
+    private void OnDrawGizmos() {
+        if (!Application.isPlaying) {
+            return;
+        }
+
+        for (int i = 0; i < _particles.Length; i++) {
+            var p = _particles[i];
+            var pos = new float3(pscalar.ToFloat(p.position.x) * 2f, pscalar.ToFloat(p.position.y) * 2f, 0f);
+            var vel = new float3(vscalar.ToFloat(p.velocity.x) * 2f, vscalar.ToFloat(p.velocity.y) * 2f, 0f);
+
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawSphere(pos, 0.05f);
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawRay(pos, vel);
+        }
+    }
+
+    public struct Particle {
+        public position position;
+        public velocity velocity;
+    }
+}
--- Assets/Scripts/StableOscillator.cs
+++ Assets/Scripts/StableOscillator.cs
@@ -1,3 +1,85 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:493dfc19e9c5dc8d334a16bfec4ada6f297be4a0a056a103f3cb12c782c3a8c8
-size 2170
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ramjet.Math.FixedPoint;
+using Unity.Mathematics;
+using Unity.Collections;
+
+/*
+Was alerted to this through @mmalex's tweet, which spawned an interesting thread
+
+Todo:
+
+- Types
+
+c, s can anything away from 0 that you want. Unitary, fractional,
+integer, doesn't matter. Energy will be preserved.
+
+Setting a to 1 means we loop the square, and we are definitely no
+longer a circle or ellipse. Still stable though
+ */
+
+
+public class StableOscillator : MonoBehaviour
+{
+    private struct Osc {
+        public qs1_14 c;
+        public qs1_14 s;
+    }
+    
+    private qu1_7 a;
+
+    private NativeArray<Osc> _oscStates;
+    private int _head;
+
+    void Start()
+    {
+        _oscStates = new NativeArray<Osc>(128, Allocator.Persistent);
+
+        _head = 0;
+        _oscStates[_head] = new Osc {
+            c = qs1_14.FromInt(1),
+            s = qs1_14.FromInt(0)
+        };
+
+        a = qu1_7.FromFloat(0.01f);
+        // a = new qs15_16(256 * 31 + 128 + 5); // Allllmost hitting the same phase every time
+    }
+
+    private void OnDestroy() {
+        _oscStates.Dispose();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var headOsc = _oscStates[_head];
+        _head = (_head+1)%_oscStates.Length;
+        var c = headOsc.c - headOsc.s * a;
+        var s = headOsc.s + c * a;
+        _oscStates[_head] = new Osc {
+            c = c,
+            s = s
+        };
+
+    }
+
+    void OnDrawGizmos() {
+        for (int i = 0; i < _oscStates.Length; i++) {
+            var idx = (_head + 1 + i) % _oscStates.Length;
+            var pos = new float3(
+                qs1_14.ToFloat(_oscStates[idx].c),
+                qs1_14.ToFloat(_oscStates[idx].s),
+                0f
+            );
+
+            var color = i == _oscStates.Length - 1 ?
+                Color.magenta :
+                new Color(1,1,1, i / (float)_oscStates.Length);
+
+            Gizmos.color = color;
+            Gizmos.DrawLine(Vector3.zero, pos);
+            Gizmos.DrawSphere(pos, 0.05f);
+        }
+    }
+}
