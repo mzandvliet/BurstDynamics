45bd9a233e48a1d1d3d018aa1add21cec3079455
m_zandvliet <martijn@ramjetanvil.com>
2019-09-11 23:47:22 +0200

    Super basic intersection tests, and reflection of velocity upon collision.

--- Assets/Scripts/FixedPoint2DPhysics.cs
+++ Assets/Scripts/FixedPoint2DPhysics.cs
@@ -44,6 +44,13 @@ Rust could handle if you write your code wrapped in a
 macro that performs analysis. I guess you could do the
 same with a Roslyn preprocessing step.
 
+Please Add:
+- Casting operators
+- >=, <=
+- Full interoperability with int, uint, short, byte
+- Only generate rings for types marked as in use through
+config file
+
 === Projective Geometry ===
 
 We're trying some projective geometry for working with
@@ -96,6 +103,17 @@ below for [c,x,y]
 Still, we might be able to find a better fusion of projective
 and fixed point arithmetic. Gotta keep experimenting.
 
+There's a kind of rolling normalization you can do. Since
+and projective vector is unchanged if multiplied by a scalar,
+if you quickly find good scalars that keep numbers in check...
+
+====
+
+Todo:
+- Compare approaches for collision functions
+    - Notably: first inverse-transforming B to the space of A
+- Try finite fields, polynumbers
+
 */
 
 namespace FixedPointPhysics {
@@ -114,95 +132,19 @@ namespace FixedPointPhysics {
             _staticSegments = new NativeArray<Segment>(1, Allocator.Persistent);
 
             _bodies[0] = new Rigidbody() {
-                position = vec2_qs19_12.FromInt(1, 10),
-                velocity = vec2_qs6_9.FromFloat(0f, 1f)
+                position = vec2_qs19_12.FromInt(-8, 10),
+                velocity = vec2_qs6_9.FromFloat(3f, 1f)
             };
 
             _staticSegments[0] = new Segment() {
                 a = vec2_qs19_12.FromInt(-10, 0),
                 b = vec2_qs19_12.FromInt(10, 0)
             };
-
-            TestProjectiveGeometry();
         }
 
-        private void TestProjectiveGeometry() {
-            // Should meet at [1, 0]
-
-            var a_a = new vec2_proj(1, 10, 0);
-            var a_b = new vec2_proj(1, -10, 0);
-            var b_a = new vec2_proj(1, 1, -10);
-            var b_b = new vec2_proj(1, 1, 10);
-
-            var joinA = JoinMeet(a_a, a_b);
-            var joinB = JoinMeet(b_a, b_b);
-            var meet = JoinMeet(joinA, joinB);
-
-            Debug.Log(meet.x / (float)meet.c + ", " + meet.y / (float)meet.c);
-
-            // The above trivial version with small integers works fine
-
-            var a_a_fp = vec3_qs19_12.FromInt(10, 2, 1);
-            var a_b_fp = vec3_qs19_12.FromInt(-10, 2, 1);
-            var b_a_fp = vec3_qs19_12.FromInt(1, -10, 1);
-            var b_b_fp = vec3_qs19_12.FromInt(1, 10, 1);
-
-            var joinA_fp = JoinMeet(a_a_fp, a_b_fp);
-            var joinB_fp = JoinMeet(b_a_fp, b_b_fp);
-            var meet_fp = JoinMeet(joinA_fp, joinB_fp);
-
-            Debug.Log(meet_fp.x / meet_fp.z + ", " + meet_fp.y / meet_fp.z);
-
-            // This did effectively the same thing with fixed point numbers
-
-            // Let's check some point-line incidences
-            var incidenceHi = Incidence(joinA_fp, vec3_qs19_12.FromInt(-1, 5, 1)); // point above line
-            var incidenceOn = Incidence(joinA_fp, vec3_qs19_12.FromInt( 1, 2, 1)); // point on line
-            var incidenceLo = Incidence(joinA_fp, vec3_qs19_12.FromInt( 5, 1, 1)); // point below line
-            Debug.Log(incidenceHi);
-            Debug.Log(incidenceOn); 
-            Debug.Log(incidenceLo);
-
-            /*
-            Works well! Appears to work great as a distance-to-line function, too
-            
-            Important though: the returned values are proportional to distance, but
-            scaled by the length of the segment that is checked against. E.g. for a
-            segment of length 20, with a point -2 units away from it, the function will
-            return -40.
-            */
-        }
-
-        private struct vec2_proj {
-            public int c;
-            public int x;
-            public int y;
-
-            public vec2_proj(int c, int x, int y) {
-                this.c = c;
-                this.x = x;
-                this.y = y;
-            }
-        }
-
-        private static vec2_proj JoinMeet(vec2_proj a, vec2_proj b) {
-            return new vec2_proj(
-                a.x * b.y - a.y * b.x,
-                a.y * b.c - a.c * b.y,
-                a.c * b.x - a.x * b.c
-            );
-        }
-
-        private static vec3_qs19_12 JoinMeet(vec3_qs19_12 a, vec3_qs19_12 b) {
-            return new vec3_qs19_12(
-                a.y * b.z - a.z * b.y,
-                a.z * b.x - a.x * b.z,
-                a.x * b.y - a.y * b.x
-            );
-        }
-
-        private static qs19_12 Incidence(vec3_qs19_12 a, vec3_qs19_12 b) {
-            return a.z * b.z + a.x * b.x + a.y * b.y;
+        private void OnDestroy() {
+            _bodies.Dispose();
+            _staticSegments.Dispose();
         }
 
         private void Update() {
@@ -214,17 +156,95 @@ namespace FixedPointPhysics {
 
                 AddAssign(ref body.position, Shr(body.velocity, ShiftToFrameDelta));
 
+                var bodySegment = new Segment {
+                    a = body.position + vec2_qs19_12.FromFloat(0f, 0.25f),
+                    b = body.position - vec2_qs19_12.FromFloat(0f, 0.25f),
+                };
+
                 for (int s = 0; s < _staticSegments.Length; s++) {
+                    var seg = _staticSegments[s];
                     
+                    if (Segment.Intersect(bodySegment, seg)) {
+                        /* 
+                        * Note that the normal can be any length we want, so we might as well
+                        * ensure that its magnitude is something helpful
+                        * 
+                        * todo: correct position, so we don't slide through eventually
+                         */
+
+                        var segDelta = seg.b - seg.a;
+                        var segNormal = new vec2_qs19_12(segDelta.y * -1, segDelta.x);
+
+                        var velocity = Cast(body.velocity);
+                        velocity = velocity - segNormal * (vec2_qs19_12.dot(velocity * qs19_12.FromInt(2), segNormal) / vec2_qs19_12.dot(segNormal, segNormal));
+
+                        body.velocity = Cast(velocity);
+                    }
                 }
                 
-                if (body.position.y < new qs19_12(0)) {
-                    body.velocity = body.velocity * qs6_9.FromInt(-1);
-                    body.position.y = qs19_12.Zero;
-                }
-
                 _bodies[i] = body;
             }
+        }
+
+        private void OnDrawGizmos() {
+            DrawPhysicsState();
+        }
+
+        private void DrawPhysicsState() {
+            Gizmos.matrix = Matrix4x4.TRS(
+                Vector3.zero,
+                Quaternion.identity,
+                Vector3.one
+            );
+            Gizmos.color = Color.white;
+
+            for (int i = 0; i < _staticSegments.Length; i++) {
+                var segment = _staticSegments[i];
+
+                var posA = new float3(
+                    qs19_12.ToFloat(segment.a.x),
+                    qs19_12.ToFloat(segment.a.y),
+                    0f);
+
+                var posB = new float3(
+                    qs19_12.ToFloat(segment.b.x),
+                    qs19_12.ToFloat(segment.b.y),
+                    0f);
+
+                Gizmos.DrawLine(posA, posB);
+            }
+
+            for (int i = 0; i < _bodies.Length; i++) {
+                var body = _bodies[i];
+
+                var pos = new float3(
+                    qs19_12.ToFloat(body.position.x),
+                    qs19_12.ToFloat(body.position.y),
+                    0f);
+
+                var boxTransform = Matrix4x4.TRS(
+                    pos,
+                    Quaternion.identity,
+                    new Vector3(1f, 0.5f, 1f)
+                );
+                Gizmos.matrix = boxTransform;
+
+                Gizmos.DrawCube(Vector3.zero, Vector3.one);
+            }
+        }
+
+        private static vec2_qs19_12 Cast(vec2_qs6_9 v) {
+            return new vec2_qs19_12(
+                new qs19_12(v.x.v >> 3),
+                new qs19_12(v.y.v >> 3)
+            );
+        }
+
+        private static vec2_qs6_9 Cast(vec2_qs19_12 v) {
+            return new vec2_qs6_9(
+                new qs6_9((short)(v.x.v << 3)),
+                new qs6_9((short)(v.y.v << 3))
+            );
         }
 
         private static vec2_qs3_12 Shr(vec2_qs6_9 v, int shift) {
@@ -240,72 +260,62 @@ namespace FixedPointPhysics {
             a.y.v = (a.y.v + b.y.v);
         }
 
-        private void OnDrawGizmos() {
-            for (int i = 0; i < _bodies.Length; i++) {
-                var body = _bodies[i];
+        private static vec3_qs19_12 JoinMeet(vec3_qs19_12 a, vec3_qs19_12 b) {
+            return new vec3_qs19_12(
+                a.y * b.z - a.z * b.y,
+                a.z * b.x - a.x * b.z,
+                a.x * b.y - a.y * b.x
+            );
+        }
 
-                var pos = new float3(
-                    qs19_12.ToFloat(body.position.x),
-                    qs19_12.ToFloat(body.position.y),
-                    0f);
+        private static qs19_12 Incidence(vec3_qs19_12 a, vec3_qs19_12 b) {
+            return a.z * b.z + a.x * b.x + a.y * b.y;
+        }
 
-                var boxTransform = Matrix4x4.TRS(
-                    pos,
-                    Quaternion.identity,
-                    new Vector3(1f,0.5f,1f)
-                );
-                Gizmos.matrix = boxTransform;
+        public struct Rigidbody {
+            public vec2_qs19_12 position;
+            public vec2_qs6_9 velocity;
+        }
 
-                Gizmos.DrawCube(Vector3.zero, Vector3.one);
+        public struct Segment {
+            public vec2_qs19_12 a;
+            public vec2_qs19_12 b;
+
+            public static bool Intersect(Segment a, Segment b) {
+                /*
+                  This uses some projective geometry to find
+                  intersection. But I'm newbie.
+                 */
+
+                var a_a_proj = new vec3_qs19_12(a.a.x, a.a.y, qs19_12.One);
+                var a_b_proj = new vec3_qs19_12(a.b.x, a.b.y, qs19_12.One);
+                var b_a_proj = new vec3_qs19_12(b.a.x, b.a.y, qs19_12.One);
+                var b_b_proj = new vec3_qs19_12(b.b.x, b.b.y, qs19_12.One);
+
+                var joinA_proj = JoinMeet(a_a_proj, a_b_proj);
+                var joinB_proj = JoinMeet(b_a_proj, b_b_proj);
+                var meet_proj = JoinMeet(joinA_proj, joinB_proj);
+
+                /*
+                Now we know the intersection of the lines, but still need to find
+                whether the point is bounded by the ends of the two segments.
+
+                For this we use two dot products and four comparisons, back in
+                Cartesian space. Could be better?
+                */
+
+                var deltaA = a.b - a.a;
+                var deltaB = b.b - b.a;
+                var meet = new vec2_qs19_12(meet_proj.x / meet_proj.z, meet_proj.y / meet_proj.z);
+                var dotA = vec2_qs19_12.dot(deltaA, meet - a.a);
+                var dotB = vec2_qs19_12.dot(deltaB, meet - b.a);
+
+                return 
+                    dotA > qs19_12.Zero && dotA < vec2_qs19_12.lengthsq(deltaA) &&
+                    dotB > qs19_12.Zero && dotB < vec2_qs19_12.lengthsq(deltaB);
             }
         }
-
-        private void OnDestroy() {
-            _bodies.Dispose();
-        }
     }
 
-    public struct Rigidbody {
-        public vec2_qs19_12 position;
-        public vec2_qs6_9 velocity;
-    }
-
-    public struct Segment {
-        public vec2_qs19_12 a;
-        public vec2_qs19_12 b;
-
-        public static bool Intersect(Segment a, Segment b) {
-            /*
-                We can try doing this with projective coordinates
-
-                https://youtu.be/lfTX00pB1L8?t=1065
-
-                Through Wildberger's lectures, we know we could take
-                rational x and y coordinates, and find a projective
-                value to multiply by, such that [c, x, y] can all
-                be integers.
-
-                We can then find the join between two points of a
-                segment.
-
-                And from two of those we can find their meet.
-
-                Here's a potential trick, then: since we're working
-                in fixed point, everything is already integers, and
-                we could interpret the Scale value as the projective
-                coordinate's value.
-
-                Very interesting confluence of fixed point arithmetic
-                and projective geometry if that works out. :)
-
-                Let's try...
-             */
-
-            return false;
-        }
-    }
-
-    public struct BoxCollider {
-        vec2_qu0_16 size;
-    }
+    
 }
